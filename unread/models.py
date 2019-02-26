af50fbc4720938f8bb0c54df736ee1c8eed5d551
Anders Hovmöller <boxed@killingar.net>
2019-02-26 04:53:50 +0100

    .

--- unread/models.py
+++ unread/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from tri.token import TokenContainer, Token, TokenAttribute
 
 
 class Model(models.Model):
@@ -35,14 +36,22 @@ class UserTime(Model):
         return f'UserTime for {self.user} {self.system}/{self.data}: {self.time}'
 
 
-SUBSCRIPTION_TYPES = ('active', 'passive')
+class SubscriptionType(Token):
+    name = TokenAttribute()
+    display_name = TokenAttribute(value=lambda name, **_: name.title())
+
+
+class SubscriptionTypes(TokenContainer):
+    active = SubscriptionType()
+    passive = SubscriptionType()
 
 
 class Subscription(Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, db_index=True)
     system = models.CharField(max_length=255, db_index=True)
     data = models.BigIntegerField(db_index=True)
-    subscription_type = models.CharField(max_length=50, choices=SUBSCRIPTION_TYPES)
+    # noinspection PyTypeChecker
+    subscription_type = models.CharField(max_length=50, choices=[(x.name, x.display_name) for x in SubscriptionTypes], db_index=True)
 
     class Meta:
         unique_together = ('user', 'system', 'data')
