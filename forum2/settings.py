f037526dbdb011ea026d2f531dfd2d6702e8dd8b
Anders Hovmöller <anders.hovmoller@trioptima.com>
2019-03-08 06:25:49 +0100

    .

--- forum2/settings.py
+++ forum2/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import jinja2
+# import jinja2
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
