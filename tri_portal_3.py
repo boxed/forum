85a2bb694f131243a281c3a02521546db356fbc3
Anders Hovmöller <anders.hovmoller@trioptima.com>
2019-06-27 15:57:31 +0200

    WIP

--- tri_portal_3.py
+++ tri_portal_3.py
@@ -1,6 +1,5 @@
 import os
 
-from bs4 import BeautifulSoup
 from django.utils.html import format_html
 from tri_declarative import (
     dispatch,
@@ -21,10 +20,11 @@ class BoundContent(RefinableObject):
     @dispatch(
         children=EMPTY,
     )
-    def __init__(self, *, name=None, tag=None, attrs=None, children, content=None, **kwargs):
+    def __init__(self, *, name=None, tag=None, no_end_tag=False, attrs=None, children, content=None, **kwargs):
         self.name = name
         self.attrs = attrs
         self.tag = tag
+        self.no_end_tag = no_end_tag
         self.children = {}
         self.content = content or ''
         for k, v in children.items():
@@ -38,7 +38,7 @@ class BoundContent(RefinableObject):
     @refinable
     def render_children(bound_content, request):
         return format_html(
-            '{}\n\n' * len(bound_content.children),
+            '{}\n' * len(bound_content.children),
             *[
                 x.render2(request) if isinstance(x, BoundContent) else str(x)
                 for x in bound_content.children.values()
@@ -51,7 +51,23 @@ class BoundContent(RefinableObject):
         rendered_children = bound_content.render_children(bound_content=bound_content, request=request)
 
         if bound_content.tag:
-            return format_html('<{}{}>{}{}</{}>', bound_content.tag, render_attrs(bound_content.attrs), bound_content.content, rendered_children, bound_content.tag)
+            if not bound_content.no_end_tag:
+                return format_html(
+                    '<{}{}>{}{}{}</{}>',
+                    bound_content.tag,
+                    render_attrs(bound_content.attrs),
+                    '\n' if rendered_children else '',
+                    bound_content.content,
+                    rendered_children,
+                    bound_content.tag,
+                )
+            else:
+                assert not bound_content.content and not rendered_children
+                return format_html(
+                    '<{}{}>',
+                    bound_content.tag,
+                    render_attrs(bound_content.attrs),
+                )
         else:
             return bound_content.content + rendered_children
 
@@ -77,7 +93,7 @@ actual = bind(
         baz='baz',
     )
 ).render2(request).strip()
-assert actual == 'foo\n\nbar\n\nbaz', actual
+assert actual == 'foo\nbar\nbaz', actual
 
 
 actual = bind(
@@ -89,7 +105,7 @@ actual = bind(
         baz__attrs__class__baz=True,
     ),
 ).render2(request).strip()
-assert actual == 'foo\n\nbar\n\n<div class="baz">baz</div>', actual
+assert actual == 'foo\nbar\n<div class="baz">baz</div>', actual
 
 
 actual = bind(
@@ -103,7 +119,7 @@ actual = bind(
         baz__attrs__class__baz=True,
     ),
 ).render2(request).strip()
-assert actual == '<div>foo\n\nbar\n\n<div class="baz">baz</div>\n\n</div>', actual
+assert actual == '<div>\nfoo\nbar\n<div class="baz">baz</div>\n</div>', actual
 
 
 actual = bind(render=lambda bound_content, request: 'foo').render2(request) == 'foo'
@@ -112,6 +128,7 @@ actual = bind(render=lambda bound_content, request: 'foo').render2(request) == '
 @dispatch(
     attrs__rel='stylesheet',
     tag='link',
+    no_end_tag=True,
 )
 def stylesheet(href, **kwargs):
     return Namespace(
@@ -138,10 +155,12 @@ foo = dict(
         ),
         favicon=dict(
             tag='link',
+            no_end_tag=True,
             attrs=dict(id="favicon", rel="shortcut icon", type="image/png", href="/static/killing-icon.png")
         ),
         viewport=dict(
             tag='meta',
+            no_end_tag=True,
             attrs=dict(name="viewport", content="width=device-width, initial-scale=1, maximum-scale=1"),
         )
     ),
@@ -149,17 +168,19 @@ foo = dict(
 
 actual = bind(**foo).render2(request).strip()
 
+print(actual)
+
 expected = """<head>
-    <title>~~title~~</title>
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" href="/static/forum-minimalist.css" >
-    <script type="text/JavaScript" src="/static/forum.js"></script>
-    <link id="favicon" rel="shortcut icon" type="image/png" href="/static/killing-icon.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>~~title~~</title>
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic" rel="stylesheet">
+<link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet">
+<link href="/static/forum-minimalist.css" rel="stylesheet">
+<script src="/static/forum.js" type="text/JavaScript"></script>
+<link href="/static/killing-icon.png" id="favicon" rel="shortcut icon" type="image/png">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 </head>"""
 
-assert BeautifulSoup(actual, 'html.parser').prettify() == BeautifulSoup(expected, 'html.parser').prettify()
+assert actual == expected
 
 
 assert 'testasdtest' in bind(**foo, children__css__attrs__foo='testasdtest').render2(request)
